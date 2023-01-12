335b1b5e870c49a9b7c330c9fe484e28d94ceef5
Moulik <moulikbudhiraja@gmail.com>
2023-01-12 12:51:46 -0500

    Added debug print for socket url

--- client/src/service/socket.tsx
+++ client/src/service/socket.tsx
@@ -3,4 +3,4 @@ import { DEV_SOCKET_URL, SOCKET_URL } from "../config";
 
 export const socket = io(window.location.origin === "https://tank-trample.budhiraja.ca" ? SOCKET_URL : DEV_SOCKET_URL);
 
-
+console.log("Socket URL: ", window.location.origin === "https://tank-trample.budhiraja.ca" ? SOCKET_URL : DEV_SOCKET_URL)
