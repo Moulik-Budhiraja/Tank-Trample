10dd3e7df87d3bfb9edbd6e7db11268f42fa2635
Moulik <moulikbudhiraja@gmail.com>
2023-01-13 18:20:33 -0500

    Developed class structure to manage game

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,2 +1,2 @@
-export const SOCKET_URL = 'http://tank-trample-server.budhiraja.ca';
+export const SOCKET_URL = 'https://tank-trample-server.budhiraja.ca';
 export const DEV_SOCKET_URL = 'http://localhost:3001';
