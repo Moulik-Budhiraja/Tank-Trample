e3d22982084d2b67a1263c80355f57dc95836c72
Moulik <moulikbudhiraja@gmail.com>
2023-01-12 21:33:59 -0500

    Fixed Cors Issue for dev and prod

--- client/src/service/socket.tsx
+++ client/src/service/socket.tsx
@@ -2,5 +2,5 @@ import io from "socket.io-client";
 import { DEV_SOCKET_URL, SOCKET_URL } from "../config";
 
 console.log("Socket URL: ", window.location.origin === "https://tank-trample.budhiraja.ca" ? SOCKET_URL : DEV_SOCKET_URL)
-export const socket = io(SOCKET_URL);
+export const socket = io(window.location.origin === "https://tank-trample.budhiraja.ca" ? SOCKET_URL : DEV_SOCKET_URL);
 
