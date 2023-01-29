bb37514c30938f67547280aa98d4cd0733744b11
Tony Liu <tony0818abc@gmail.com>
2023-01-29 01:56:25 -0500

    Added projectile collisions with maze walls

--- server/common/structures/projectiles.ts
+++ server/common/structures/projectiles.ts
@@ -23,25 +23,43 @@ export class Projectile {
         this.timeFired = Date.now();
     }
 
-    // ! NEEDS WORK
     update(map: Map) {
+        let oldNode = map.getNodeFromPos(this.position);
+        let oldPos = this.position.copy();
         this.position.updateByVelocity(this.velocity);
 
-        if (this.position.x < 0) {
-            this.position.x = 0;
-            this.velocity.x *= -1;
+        let newNode = map.getNodeFromPos(this.position);
+        let newPos = this.position.copy();
+        if (oldNode === null || newNode === null) {}
+        else if (oldNode === newNode || oldNode.connected.includes(newNode)) {
+            return;
+        } 
+        let slope = (newPos.y - oldPos.y) / (newPos.x - oldPos.x);
+        let h1 = Math.floor(oldPos.y / map.scale) * map.scale;
+        let h2 = Math.ceil(oldPos.y / map.scale) * map.scale;
+        let v1 = Math.floor(oldPos.x / map.scale) * map.scale;
+        let v2 = Math.ceil(oldPos.x / map.scale) * map.scale;
+
+        let x1 = (h1 - oldPos.y) / slope + oldPos.x;
+        let x2 = (h2 - oldPos.y) / slope + oldPos.x;
+        let y1 = (v1 - oldPos.x) * slope + oldPos.y;
+        let y2 = (v2 - oldPos.x) * slope + oldPos.y;
+
+        if (v1 < x1 && x1 < v2 && newPos.y < h1 && h1 < oldPos.y) {
+            this.position = new Position(x1, h1);
+            this.velocity.y = -this.velocity.y;
         }
-        if (this.position.y < 0) {
-            this.position.y = 0;
-            this.velocity.y *= -1;
+        if (v1 < x2 && x2 < v2 && oldPos.y < h2 && h2 < newPos.y) {
+            this.position = new Position(x2, h2);
+            this.velocity.y = -this.velocity.y;
         }
-        if (this.position.x > map.width * map.scale) {
-            this.position.x = map.width * map.scale;
-            this.velocity.x *= -1;
+        if (h1 < y1 && y1 < h2 && newPos.x < v1 && v1 < oldPos.x) {
+            this.position = new Position(v1, y1);
+            this.velocity.x = -this.velocity.x;
         }
-        if (this.position.y > map.height * map.scale) {
-            this.position.y = map.height * map.scale;
-            this.velocity.y *= -1;
+        if (h1 < y2 && y2 < h2 && oldPos.x < v2 && v2 < newPos.x) {
+            this.position = new Position(v2, y2);
+            this.velocity.x = -this.velocity.x;
         }
     }
 
